9c15fab5a5154667088d2f807335898c7588a4c2
Afilehit <33673714+Afilehit@users.noreply.github.com>
2020-08-09 15:31:43 +0900

    Add files via upload

--- vkbridge.js
+++ vkbridge.js
@@ -1,18 +1,15 @@
-
-
-var module = require("/index.js");
-
-module.f(); // запуск экспортируемой функции модуля
-console.log(module.obj);
-
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/client/game.html');
+	res.sendFile(__dirname + '/client/index.html');
 
 });
 app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
 
+//var module = require("/index.js");
+
+// module.f(); // запуск экспортируемой функции модуля
+// console.log(module.obj);
